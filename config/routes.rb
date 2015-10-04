4816f158b339736d83e13c0d05c394a33c45c665
Jimi Vyas <jimipvyas@gmail.com>
2015-10-04 14:54:21 -0400

    fix model n controller u know part 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,55 @@ Rails.application.routes.draw do
      resources :products
     end
 
+    def index
+      @quoteunquoteyelps = Quoteunquoteyelp.all
+    end
+
+    def show
+      @quoteunquoteyelp = Quoteunquoteyelp.find(params[:id])
+    end
+
+    def new
+      @quoteunquoteyelp = Quoteunquoteyelp.new
+    end
+
+    def edit
+      @quoteunquoteyelp = Quoteunquoteyelp.find(params[:id])
+    end
+
+    def create
+      @quoteunquoteyelp = Quoteunquoteyelp.new(quoteunquoteyelp_params)
+
+      if @quoteunquoteyelp.save
+        redirect_to quoteunquoteyelps_url
+      else
+        render :new
+      end
+    end
+
+    def update
+      @quoteunquoteyelp = Quoteunquoteyelp.find(params[:id]))
+  
+      if @quoteunquoteyelp.update_attributes(quoteunquoteyelp_params)
+        redirect_to quoteunquoteyelp_path(@quoteunquoteyelp)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @quoteunquoteyelp = Quoteunquoteyelp.find(params[:id])
+      @quoteunquoteyelp.destroy
+      redirect_to quoteunquoteyelps_path
+    end
+
+    private
+    def quoteunquoteyelp_params
+      params.require(:quoteunquoteyelp).permit(:name, :description, :year)
+    end
+  end
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

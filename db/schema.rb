5aa4056bc2fef7ebcf4e5aca406165ba65f287d2
Jimi Vyas <jimipvyas@gmail.com>
2015-10-04 16:43:34 -0400

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004182634) do
+ActiveRecord::Schema.define(version: 20151004202535) do
 
   create_table "quoteunquoteyelps", force: true do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20151004182634) do
     t.integer  "year"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
+    t.string   "photourl"
   end
 
 end
